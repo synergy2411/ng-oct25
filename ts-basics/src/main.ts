6ea235f21542d4438885876e9712473dd56fe893
Sumit Khandelwal <synergy2411@outlook.com>
2025-10-14 13:30:26 +0530

    generics added

--- ts-basics/src/main.ts
+++ ts-basics/src/main.ts
@@ -1,103 +1,233 @@
-// // Explicit Types
-// let x: string;
+// // // Explicit Types
+// // let x: string;
 
-// x = "Hello TypeScript!";
+// // x = "Hello TypeScript!";
 
-// console.log("X : ", x);
+// // console.log("X : ", x);
 
-// console.log(typeof x);
+// // console.log(typeof x);
 
-// // Implicit Types - Recommended
-// let y = 200;
+// // // Implicit Types - Recommended
+// // let y = 200;
 
-// console.log("Type of Y:", typeof y);
+// // console.log("Type of Y:", typeof y);
 
-// // Custom Type with 'type' keyword
+// // // Custom Type with 'type' keyword
 
-// type MyCustomType = string | number;
+// // type MyCustomType = string | number;
 
-// let x: MyCustomType;
+// // let x: MyCustomType;
 
-// x = "Twenty Three";
+// // x = "Twenty Three";
 
-// x = 23;
+// // x = 23;
 
-// // TYPES IN TYPESCRIPT
+// // // TYPES IN TYPESCRIPT
 
-let varString: string;
-varString = "Hello World";
+// let varString: string;
+// varString = "Hello World";
 
-let varBoolean: boolean;
-varBoolean = true;
+// let varBoolean: boolean;
+// varBoolean = true;
 
-let varDate: Date;
-varDate = new Date("Dec 20, 2024");
+// let varDate: Date;
+// varDate = new Date("Dec 20, 2024");
 
-// let varArray: string[];
-let varArray: Array<string>;
-varArray = ["Monica", "Joey", "Chandler"];
+// // let varArray: string[];
+// let varArray: Array<string>;
+// varArray = ["Monica", "Joey", "Chandler"];
 
-let varFunction: () => void;
-varFunction = () => console.log("Function - does not return");
+// let varFunction: () => void;
+// varFunction = () => console.log("Function - does not return");
 
-let varObject: { firstName: string; lastName: string };
-varObject = { firstName: "John", lastName: "Doe" };
+// let varObject: { firstName: string; lastName: string };
+// varObject = { firstName: "John", lastName: "Doe" };
 
-let varAny: any; // NOT RECOMMENDED
-varAny = "String Value";
-varAny = 101;
-varAny = true;
-varAny = [];
-varAny = {};
-varAny = () => {};
+// let varAny: any; // NOT RECOMMENDED
+// varAny = "String Value";
+// varAny = 101;
+// varAny = true;
+// varAny = [];
+// varAny = {};
+// varAny = () => {};
 
-let varUnknow: unknown; // Perform type-checking before any operation
-varUnknow = "Hello There";
-if (typeof varUnknow === "string") {
-  console.log("Length : ", varUnknow.length);
+// let varUnknow: unknown; // Perform type-checking before any operation
+// varUnknow = "Hello There";
+// if (typeof varUnknow === "string") {
+//   console.log("Length : ", varUnknow.length);
+// }
+// varUnknow = 201;
+// varUnknow = true;
+
+// let varUnion: string | number;
+// varUnion = "Twenty";
+// varUnion = 20;
+
+// let varTuple: [string, number];
+// varTuple = ["John Doe", 21];
+
+// let varUndefined: string | null;
+// varUndefined = null;
+// varUndefined = "Hello There";
+
+// function doSomething(): void {
+//   console.log("Void function");
+// }
+
+// function throwError(): never {
+//   throw new Error("Throwing the error");
+// }
+
+// // // Custom Types
+
+// //// - type keyword
+// //// - Classes
+// //// - Interfaces
+
+// type AgeType = string | number;
+
+// //// Interface as a type
+// interface Todo {
+//   label: string;
+//   status: boolean;
+// }
+
+// let todoCollection: Todo[] = [];
+// todoCollection.push({ label: "Shopping", status: true });
+
+// //// Classes as a type
+// class Foo {}
+// class Bar {}
+
+// let foo: Foo = new Foo();
+// let bar: Bar = new Bar();
+
+// //// Classes
+
+// class Person {
+//   //// private name: string;
+//   //// private age: number;
+
+//   //// constructor(name: string, age: number) {
+//   ////   this.name = name;
+//   ////   this.age = age;
+//   //// }
+
+//   constructor(private name: string, private age: number) {}
+
+//   public getDetails(): string {
+//     return `Name : ${this.name} | Age : ${this.age}`;
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(name: string, age: number, private studId: string) {
+//     super(name, age);
+//   }
+
+//   // Method Overriding
+//   getDetails() {
+//     return `${super.getDetails()} | Student ID : ${this.studId}
+//     `;
+//   }
+// }
+
+// const personOne = new Student("Monica Geller", 21, "S001");
+// const personTwo = new Student("Chandler Bing", 24, "S002");
+
+// console.log(personOne.getDetails());
+// console.log(personTwo.getDetails());
+
+// // // Getter and Setter
+// class Animal {
+//   private _species!: string;
+//   private static numberOfAnimals = 0;
+
+//   constructor() {
+//     Animal.numberOfAnimals += 1;
+//   }
+
+//   get species() {
+//     return this._species;
+//   }
+
+//   set species(value: string) {
+//     this._species = value;
+//   }
+
+//   static getTotalAnimals() {
+//     return Animal.numberOfAnimals;
+//   }
+// }
+
+// let bunny = new Animal();
+// bunny.species = "Rabbit";
+// console.log("Bunny Species is : ", bunny.species);
+
+// let kitty = new Animal();
+// kitty.species = "Cat";
+// console.log("Kitty Species is : ", kitty.species);
+
+// console.log("Total animals so far : ", Animal.getTotalAnimals());
+
+//// ABSTRACT CLASSES
+
+abstract class Receipe {
+  abstract collectIngredients(): void;
+  abstract prepareTheReceipte(): void;
+  abstract cleanUp(): void;
+
+  execute() {
+    this.collectIngredients();
+    this.prepareTheReceipte();
+    this.cleanUp();
+  }
 }
-varUnknow = 201;
-varUnknow = true;
 
-let varUnion: string | number;
-varUnion = "Twenty";
-varUnion = 20;
-
-let varTuple: [string, number];
-varTuple = ["John Doe", 21];
-
-let varUndefined: string | null;
-varUndefined = null;
-varUndefined = "Hello There";
-
-function doSomething(): void {
-  console.log("Void function");
+class Tea extends Receipe {
+  collectIngredients(): void {
+    console.log("Tea leafs, sugar, water, ginger");
+  }
+  prepareTheReceipte(): void {
+    console.log("Boil the water");
+    console.log("Add all ingredients");
+  }
+  cleanUp(): void {
+    console.log("Clean up all dishes and boiler");
+  }
 }
 
-function throwError(): never {
-  throw new Error("Throwing the error");
+const gingerTea = new Tea();
+gingerTea.execute();
+
+interface HasFormatter {
+  format: () => void;
 }
 
-// // Custom Types
-
-//// - type keyword
-//// - Classes
-//// - Interfaces
-
-type AgeType = string | number;
-
-//// Interface as a type
-interface Todo {
-  label: string;
-  status: boolean;
+class Paint implements HasFormatter {
+  format() {
+    console.log("Performs formatting");
+  }
 }
 
-let todoCollection: Todo[] = [];
-todoCollection.push({ label: "Shopping", status: true });
+// // GENERICS
+// function addAtBeginning(item: string, arr: Array<string>): Array<string> {
+//   return [item, ...arr];
+// }
 
-//// Classes as a type
-class Foo {}
-class Bar {}
+// const modifedArray = addAtBeginning("Monica", ["Joey", "Rachel", "Chandler"]);
+// console.log("Modified Array : ", modifedArray);
 
-let foo: Foo = new Foo();
-let bar: Bar = new Bar();
+function addAtBeginning<T>(item: T, arr: Array<T>): Array<T> {
+  return [item, ...arr];
+}
+
+const numberArray = addAtBeginning<number>(99, [98, 92, 89, 84]);
+console.log("Number Array : ", numberArray);
+
+const stringArray = addAtBeginning<string>("Monica", [
+  "Joey",
+  "Rachel",
+  "Chandler",
+]);
+console.log("String Array : ", stringArray);
