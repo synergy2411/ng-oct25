8ad59667b656023a8cf2c6a85e716993d38ac792
Sumit Khandelwal <synergy2411@outlook.com>
2025-10-17 10:12:00 +0530

    attribute directives added

--- frontend/src/app/components/users/user-info/user-info.component.ts
+++ frontend/src/app/components/users/user-info/user-info.component.ts
@@ -8,4 +8,19 @@ import { IUser } from '../../../model/user-model';
 })
 export class UserInfoComponent {
   @Input() user!: IUser;
+
+  myDynamicStyle = { color: 'grey', backgroundColor: 'white' };
+
+  myDynamicClasses = { 'my-feature': false, 'my-border': true };
+
+  onChangeStyle() {
+    let tempColor = this.myDynamicStyle.color;
+    this.myDynamicStyle.color = this.myDynamicStyle.backgroundColor;
+    this.myDynamicStyle.backgroundColor = tempColor;
+  }
+
+  onChangeClasses() {
+    this.myDynamicClasses['my-border'] = !this.myDynamicClasses['my-border'];
+    this.myDynamicClasses['my-feature'] = !this.myDynamicClasses['my-feature'];
+  }
 }
