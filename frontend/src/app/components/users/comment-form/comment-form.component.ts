e6cddb2f62693efa1cb4fc39c241d9db4be40f9e
Sumit Khandelwal <synergy2411@outlook.com>
2025-10-21 10:51:53 +0530

    template form added

--- frontend/src/app/components/users/comment-form/comment-form.component.ts
+++ frontend/src/app/components/users/comment-form/comment-form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-comment-form',
   templateUrl: './comment-form.component.html',
-  styleUrl: './comment-form.component.css'
+  styleUrl: './comment-form.component.css',
 })
 export class CommentFormComponent {
-
+  onSumbit(form: NgForm) {
+    console.log(form);
+  }
 }
